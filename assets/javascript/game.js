c8fdf07289ae64dc669c7e01f63180497a976a39
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2017-04-02 21:35:49 -0700

    Added logic to automatically restart game after a win or loss, cleaned up code, set clearer sequence of events, show running total of wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,43 +13,79 @@ var words = ["Terminator",
 		"Conan The Destroyer",
 		"Kindergarten Cop"];
 var currentWord = words[Math.floor(Math.random() * words.length)];
-console.log(currentWord);
 var remainingLetters = currentWord;
 var guessesRemaining = 15;
-var guessesUsed = 0;
-var guessedRight = 0;
-var guessedWrong = 0;
 var wins = 0;
 var losses = 0;
-var userGuess = 0;
+var userGuess = "";
 var correctGuessedLetters = [];
 var guessThisWord = [];
 
 
+//Display info at start of new game
+function newGameDisplay () {
+	console.log("New Game!!!");
+	//Display: "Press any key to get started!"
+	console.log("Press any key to get started!");
+		
+	//Display: Number of wins
+	console.log("Wins: " + wins);
+
+	//Display: Number of loses
+	console.log("Loses: " + losses);
+
+	//Display: Number of guesses remaining
+	console.log("Guesses remaining: " + guessesRemaining);
+}
+
 //Display: A "_ " (underscore and space) for each character in the word ( "_ _ _" for "cat")
 function hideWord (word) {
-	console.log(word);
 	for (i=0; i<word.length; i++) {
-			
-			guessThisWord[i] = "_ ";
-			
+			guessThisWord[i] = "_ ";	
 		}
 		console.log(word); 
 	}
 
-hideWord(currentWord);
+//Reset in-game stats
+function restartGame () {
+	guessesRemaining = 20;
+	userGuess = "";
+	correctGuessedLetters = [];
+	guessThisWord = [];
+	
+	//Pick new movie title
+	currentWord = words[Math.floor(Math.random() * words.length)];
+	hideWord(currentWord);
+	remainingLetters = currentWord;
+	
+	newGameDisplay();
+
+}
+
+//Starts the game
+restartGame();
+
 
 //Event listener for keyup to register when the user types a letter
 document.onkeyup = function (event) {
 	userGuess = event.key.toLowerCase();
-	guessesRemaining -= 1;
 	console.log(userGuess);
 	checkGuess(currentWord);
-	console.log("Score: " + guessedRight)
+	guessesRemaining -= 1;
+
 	console.log(guessThisWord);
 	//console.log("got that right: " + guessedRight);
 	if (guessThisWord.join("") === currentWord) {
-	console.log("You win!!!");
+		console.log("You win!!!");
+		wins += 1;
+		restartGame();
+	}
+
+	console.log(guessesRemaining);
+	if (guessesRemaining === 0) {
+		console.log("You're a loser!!!!")
+		losses +=1;
+		restartGame();
 	}
 }
 
@@ -59,50 +95,13 @@ function checkGuess (word) {
 	for (i=0; i<word.length; i++) {
 			//console.log("The current letter in the loop is: " + word[i]);
 		if (userGuess === word[i].toLowerCase()) {
+			//Display: All letters guessed so far (example: D E S H O N W S)
 			console.log(word);
 			//add correctly guessed letter to
 			guessThisWord[i] = word[i];
+			//Display: Letters guessed so far for the word ("g _ a _ e" for "game")
 			console.log(guessThisWord);
-			console.log(guessThisWord.join(""));
-			guessedRight += 1;
-			//break;
 		} 
 	}
 }
-
-
-
-//Display on the page
-	//Display: "Press any key to get started!"
-	//Display: Number of wins
-
-
-
-
-	//Display: Letters guessed so far for the word ("g _ a _ e" for "game")
-
-
-	//Display: Number of guesses remaining
-
-		//Start with 5
-
-		//Only decriment for wrong guesses
-
-
-	//Display: All letters guessed so far (example: D E S H O N W S)
-
-//User wins when all letters in the movie title have been guessed
-// if (correctGuessedLetters.toLowerCase() === currentWord.toLowerCase()) {
-// 	console.log("You win!!!");
-// }
-
-if (guessesRemaining === 0) {
-	console.log("You lose!");
-}
-	//Display wins losses: 
-
-		//Update scpre after each game
-
-	//Start new game automatically
-
 
